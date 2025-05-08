409a64d0c6f018309c7140a6e9cdf5a6a7bfa51f
DEEPYADAV-lab <yadavdeep556@gmail.com>
2025-05-09 02:14:02 +0530

    Delete Web-Development-main directory

--- Web-Development-main/alert.js
+++
@@ -1,3 +0,0 @@
-
-alert("Hello ! Welcome to JavaScript.");
-                    
--- Web-Development-main/bulb.js
+++
@@ -1,8 +0,0 @@
-function toggleLight() {
-    let lightbulb = document.getElementById("lightbulb");
-    if (lightbulb.src.includes("media/oFF_Bulb.jpg")) {
-        lightbulb.src = "media/On_Blub.jpg";
-    } else {
-        lightbulb.src = "media/oFF_Bulb.jpg";
-    }
-}
